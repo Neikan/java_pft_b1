40e52421e543cc8fc7abbbd6723a88719571813c
Eugene Sladkov <sladkov.e.m@outlook.com>
2018-12-24 15:47:05 +0300

    Функция расчета расстояния между точками изменена на метод в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -14,7 +14,8 @@ public class MyFirstProgram {
 		Rectangle r = new Rectangle(4, 6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-	/*Выполнение задания №2*/
+	/*Выполнение задания №2. Пункты 1-3*/
+		/*
 		Point p1 = new Point();
 		p1.x = 1;
 		p1.y = 1;
@@ -24,6 +25,14 @@ public class MyFirstProgram {
 		p2.y = 2;
 
 		System.out.println("Расстояние между точками c координатами (" + p1.x + ";" + p1.y + ") и (" + p2.x + ";" + p2.y + ") = " + distance(p1, p2));
+		*/
+
+		/*Выполнение задания №2*. Пункт 4 */
+		Point p1 = new Point(1,6);
+		Point p2 = new Point(6, 1);
+
+		System.out.println("Расстояние между точками c координатами (" + p1.x + ";" + p1.y + ") и (" + p2.x + ";" + p2.y + ") = " + p1.distance(p1, p2));
+
 	}
 
 	/*Лекции*/
@@ -31,9 +40,8 @@ public class MyFirstProgram {
 		System.out.println("Hello, " + somebody + "!");
 	}
 
-	/*Выполнение задания №2*/
-	public static double distance(Point p1, Point p2){
+	/*Выполнение задания №2*. Пункты 1-3/
+	/*public static double distance(Point p1, Point p2){
 		return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y)); /*Вычисляется по формуле: корень квадратный из суммы квадратов разностей соответствующих координат второй и первой точек*/
-	}
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,4 +5,14 @@ public class Point {
   public double x;
   public double y;
 
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+  /*Выполнение задания №2*/
+  public double distance(Point p1, Point p2){
+    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y)); /*Вычисляется по формуле: корень квадратный из суммы квадратов разностей соответствующих координат второй и первой точек*/
+  }
+
 }
