e64ac6531d3df5210afb4c148a824558f754aeaa
Eugene Sladkov <sladkov.e.m@outlook.com>
2018-12-24 13:46:57 +0300

    Добавлена функция расчета расстояния между точками и пример его вычисления

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,7 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
+	/*Лекции*/
 		hello("world");
 		hello("user");
 		hello("Eugene");
@@ -12,10 +13,27 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(4, 6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+	/*Выполнение задания №2*/
+		Point p1 = new Point();
+		p1.x = 1;
+		p1.y = 1;
+
+		Point p2 = new Point();
+		p2.x = 2;
+		p2.y = 2;
+
+		System.out.println("Расстояние между точками c координатами (" + p1.x + ";" + p1.y + ") и (" + p2.x + ";" + p2.y + ") = " + distance(p1, p2));
 	}
 
+	/*Лекции*/
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
+	}
+
+	/*Выполнение задания №2*/
+	public static double distance(Point p1, Point p2){
+		return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y)); /*Вычисляется по формуле: корень квадратный из суммы квадратов разностей соответствующих координат второй и первой точек*/
 	}
 
 }
