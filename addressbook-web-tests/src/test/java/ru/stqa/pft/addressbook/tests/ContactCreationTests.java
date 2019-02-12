80a79c16ab6727daae6e1bbf724549f1b23cc8b1
Eugene Sladkov <sladkov.e.m@outlook.com>
2019-02-12 13:07:02 +0300

    В ContactCreationTests добавлены выбор и прикрепление фотографии контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -11,7 +11,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test(enabled = false)
+  @Test
   public void testContactCreation() throws Exception {
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/Batman.jpg");
@@ -24,7 +24,7 @@ public class ContactCreationTests extends TestBase {
             before.withAdded(contact.withId(after.stream().mapToInt(c -> c.getId()).max().getAsInt()))));
   }
 
-  @Test
+  @Test(enabled = false)//Определен относительный пункт к файлу фотографии, добавлен тест на пр…
   public void testCurrentDir() {
     File currentDir = new File(".");
     System.out.println(currentDir.getAbsolutePath());
