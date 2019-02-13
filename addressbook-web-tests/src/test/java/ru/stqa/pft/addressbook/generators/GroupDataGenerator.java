d8666da84c46f7e19bb6b4497572079e8e4907d2
Eugene Sladkov <sladkov.e.m@outlook.com>
2019-02-13 18:26:30 +0300

    Добавлено использование библиотеки JCommander в ContactDataGenerator и GroupDataGenerator

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -10,15 +12,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
-  public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
 
-    List<GroupData> groups = generateGroups(count);
-    save(groups, file);
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
+
+  @Parameter(names = "-f", description = "target file")
+  public String file;
+
+  public static void main(String[] args) throws IOException {
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    jCommander.parse(args);
+    generator.run();
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void run() throws IOException {
+    List<GroupData> groups = generateGroups(count);
+    save(groups, new File(file));
+  }
+
+  private void save(List<GroupData> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
     for (GroupData group : groups) {
@@ -27,7 +40,7 @@ public class GroupDataGenerator {
     writer.close();
   }
 
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++) {
       groups.add(new GroupData().withName(String.format("test %s", i))
