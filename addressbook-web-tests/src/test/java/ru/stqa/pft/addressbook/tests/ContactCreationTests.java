98edf9551f1a2082004820fc7241bbf959cec875
Eugene Sladkov <sladkov.e.m@outlook.com>
2019-02-13 23:15:25 +0300

    Добавлена параметризация ContactCreationTests (часть 1)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,22 +1,33 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withFirstname("Евгений").withMiddlename("Михайлович").withLastname("Волков").withBday("12").withBmonth("Ноябрь").withByear("1987").withGroup("[не выбрано]")});
+    list.add(new Object[] {new ContactData().withFirstname("Дмитрий").withMiddlename("Дмитриевич").withLastname("Воронов").withBday("13").withBmonth("Май").withByear("1988").withGroup("[не выбрано]")});
+    list.add(new Object[] {new ContactData().withFirstname("Михаил").withMiddlename("Евгеньевич").withLastname("Кабанов").withBday("14").withBmonth("Июль").withByear("1989").withGroup("[не выбрано]")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) throws Exception {
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/Batman.jpg");
-    ContactData contact = new ContactData()
-            .withFirstname("Джон").withMiddlename("Джошович").withLastname("Смит").withAddress("Вашингтон").withPhoneMobile("+1119188900075").withEmailOne("smite@outlook.com").withBday("17").withBmonth("Декабрь").withByear("1980").withAddress2("DC").withGroup("[не выбрано]").withPhoto(photo);
     app.contact().create(contact, true);
     assertThat(app.contact().count(), equalTo(before.size() + 1));//Проверка на основе хеширования
     Contacts after = app.contact().all();
