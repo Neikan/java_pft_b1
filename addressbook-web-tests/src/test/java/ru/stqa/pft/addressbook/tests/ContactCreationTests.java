7d45efa8cf37f3ec0e579c7e333449c2700af860
Eugene Sladkov <sladkov.e.m@outlook.com>
2019-02-13 23:45:01 +0300

    Добавлена параметризация ContactCreationTests (часть 2)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,7 +5,10 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,11 +19,17 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() {
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {new ContactData().withFirstname("Евгений").withMiddlename("Михайлович").withLastname("Волков").withBday("12").withBmonth("Ноябрь").withByear("1987").withGroup("[не выбрано]")});
-    list.add(new Object[] {new ContactData().withFirstname("Дмитрий").withMiddlename("Дмитриевич").withLastname("Воронов").withBday("13").withBmonth("Май").withByear("1988").withGroup("[не выбрано]")});
-    list.add(new Object[] {new ContactData().withFirstname("Михаил").withMiddlename("Евгеньевич").withLastname("Кабанов").withBday("14").withBmonth("Июль").withByear("1989").withGroup("[не выбрано]")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData().withFirstname(split[0]).withMiddlename(split[1]).withLastname(split[2])
+              .withPhoneMobile(split[3]).withEmailOne(split[4])
+              .withBday(split[5]).withBmonth(split[6]).withByear(split[7]).withGroup(split[8])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
