ba6156f790b60dd360b7e4e51151f5669ba87100
Eugene Sladkov <sladkov.e.m@outlook.com>
2019-02-17 18:04:00 +0300

    Добавлено протоколирование выполнения тестов в TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,8 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -24,7 +22,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  Logger logger = LoggerFactory.getLogger(GroupCreationTests.class);
+
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromCsv() throws IOException {
@@ -74,7 +72,6 @@ public class GroupCreationTests extends TestBase {
 
   @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) throws Exception {
-    logger.info("Start test testGroupCreation");
     app.goTo().groupPage();
     Groups before = app.group().all();
     app.group().create(group);
@@ -93,6 +90,5 @@ public class GroupCreationTests extends TestBase {
     assertThat(app.group().count(), equalTo(before.size()));//Проверка на основе хеширования
     Groups after = app.group().all();
     assertThat(after, equalTo(before));
-    logger.info("Stop test testGroupCreation");
   }
 }
