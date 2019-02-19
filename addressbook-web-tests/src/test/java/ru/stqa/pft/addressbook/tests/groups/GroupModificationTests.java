405c5ff2cb8cc938eb4d133e02ddd2894bb42820
Eugene Sladkov <sladkov.e.m@outlook.com>
2019-02-19 15:59:27 +0300

    Проверка списков, загружаемых из БД для ContactModificationTests и GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/groups/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/groups/GroupModificationTests.java
@@ -13,21 +13,24 @@ public class GroupModificationTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
-    app.goTo().groupPage();
-    if (app.group().all().size() == 0) {//Добавлена проверка и обеспечение предусловий выполнения GroupModificationTests и GroupDeletionTests
-      app.group().create(new GroupData().withName("test4"));
+    if (app.db().groups().size() == 0) {
+      app.goTo().groupPage();
+      if (app.group().all().size() == 0) {//Добавлена проверка и обеспечение предусловий выполнения GroupModificationTests и GroupDeletionTests
+        app.group().create(new GroupData().withName("test4"));
+      }
     }
   }
 
   @Test
   public void testGroupModification() {
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData()
             .withId(modifiedGroup.getId()).withName("test9").withHeader("test12").withFooter("test13");
+    app.goTo().groupPage();
     app.group().modify(group);
     assertThat(app.group().count(), equalTo(before.size()));//Проверка на основе хеширования
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
 }
